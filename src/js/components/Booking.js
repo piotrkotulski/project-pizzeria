3fcaa321869483a07902edcd3f0285cad929fcc8
Piotr Kotulski <piotr.kotulski1986@gmail.com>
2023-11-03 00:20:17 +0100

    inputs created

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from '../components/DatePicker.js';
+import HourPicker from '../components/HourPicker.js';
 
 class Booking {
     constructor(element) {
@@ -15,12 +17,18 @@ class Booking {
 
         this.dom.peopleAmount = this.dom.wrapper.querySelector(select.booking.peopleAmount);
         this.dom.hoursAmount = this.dom.wrapper.querySelector(select.booking.hoursAmount);
+        this.dom.datePicker = this.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        this.dom.hourPicker = this.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
     }
 
     initWidgets() {
+        const thisBooking = this;
 
         this.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
         this.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
+        this.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        this.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     }
 }
 
